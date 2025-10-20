713399cad3cfd39ad4ab1304dc103d0755ee2958
quocbahuynh <quochbcontact@gmail.com>
2025-10-20 18:18:05 +0700

    test(core): add test cases for nested and array values

--- src/utils/validateTemplateParams/validateTemplateParams.test.ts
+++ src/utils/validateTemplateParams/validateTemplateParams.test.ts
@@ -1,28 +1,24 @@
 import { validateTemplateParams } from "./validateTemplateParams";
 
+
 describe('validateTemplateParams', () => {
+
   it('should not throw when templateParams is undefined', () => {
     expect(() => validateTemplateParams()).not.toThrow();
   });
 
-  it('should not throw when templateParams is a valid object', () => {
-    expect(() => validateTemplateParams({ name: 'Quoc' })).not.toThrow();
-  });
-
-   it('should not throw when templateParams has multiple keys', () => {
+  it('should not throw when templateParams is a flat object', () => {
     const params = { name: 'Quoc', age: 25, country: 'Vietnam' };
     expect(() => validateTemplateParams(params)).not.toThrow();
+  });
+
+  it('should not throw when templateParams is an empty object', () => {
+    expect(() => validateTemplateParams({})).not.toThrow();
   });
 
 
   it('should throw when templateParams is a string', () => {
     expect(() => validateTemplateParams('invalid')).toThrow(
-      'The template params have to be an object'
-    );
-  });
-
-  it('should throw when templateParams is a number', () => {
-    expect(() => validateTemplateParams(123)).toThrow(
       'The template params have to be an object'
     );
   });
@@ -33,7 +29,33 @@ describe('validateTemplateParams', () => {
     );
   });
 
-  it('should throw when templateParams is null', () => {
+  it('should throw when templateParams is a number', () => {
+    expect(() => validateTemplateParams(123)).toThrow(
+      'The template params have to be an object'
+    );
+  });
+
+  it('should throw when templateParams is a boolean', () => {
+    expect(() => validateTemplateParams(true)).toThrow(
+      'The template params have to be an object'
+    );
+  });
+
+  it('should throw when templateParams contains nested object', () => {
+    const params = { user: { name: 'Quoc' } };
+    expect(() => validateTemplateParams(params)).toThrow(
+      'The template params cannot contain nested objects'
+    );
+  });
+
+  it('should throw when templateParams contains array as value', () => {
+    const params = { items: ['a', 'b'] };
+    expect(() => validateTemplateParams(params)).toThrow(
+      'The template params cannot contain nested objects'
+    );
+  });
+
+  it('should not throw when templateParams is null', () => {
     expect(() => validateTemplateParams(null)).not.toThrow();
   });
 });
