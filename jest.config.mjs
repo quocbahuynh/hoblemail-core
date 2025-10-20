aa3184fb9251caf7f4b979aa39384541a47a2e76
quocbahuynh <quochbcontact@gmail.com>
2025-10-20 19:17:00 +0700

    config: migrate Jest config to ESM and update path aliases

---
+++ jest.config.mjs
@@ -0,0 +1,17 @@
+import { createDefaultPreset } from "ts-jest";
+
+/** @type {import("jest").Config} **/
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+export default {
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@methods/(.*)$": "<rootDir>/src/methods/$1",
+    "^@api/(.*)$": "<rootDir>/src/api/$1",
+  },
+};
